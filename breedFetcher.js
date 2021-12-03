3445ddd833749fb0111e2c6e1f847b65a90912b9
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-12-03 01:30:02 +0000

    Moved user based input to index.js, and kept fetcher function in breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,21 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const inputBreed = args[0];
 
-request(`https://api.thecatsdsapi.com/v1/breeds/search?q=${inputBreed}`, (error, response, body) => {
-  if (error) {
-    console.log(error)
-    return
-  };
-
-  const data = JSON.parse(body);
-
-  if (data[0]) {
-    console.log(data[0].description);
-  }
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
   
-  if (!data[0]) {
-    console.log("Breed not found");
-  }
-})
+    const data = JSON.parse(body);
+  
+    if (data[0]) {
+      callback(data[0].description);
+    }
+    
+    if (!data[0]) {
+      callback("Breed not found");
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
